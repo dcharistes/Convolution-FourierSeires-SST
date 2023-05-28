245c5bdb3b76f1f62f41ceab6716a83ec34260b4
dcharistes <d.charistes@gmail.com>
2023-05-28 21:17:21 +0300

    final final final

--- ask1b_2031.m
+++ ask1b_2031.m
@@ -1,7 +1,7 @@
 function ask1b_2031
 clc;clear;
 
-syms t   
+syms t;   
 x=pi^2-t^2;
 a0_c=2*(pi^2)/3;
 sum_c=0;
@@ -30,18 +30,12 @@ figure('Name','Initial function')
 fplot(t,x,[-pi,pi]), title('function x= π^2 - t^2'),
 grid on, xlim([-pi pi]), ylim([0 10]), xlabel('t'), ylabel('x'); 
 
-figure('Name',"Fourier series transformation of x (one graph)")
-fplot(t,x,[-pi,pi]);hold on;
-for l=1:5
-fplot(t,f_s_c(l),[-pi,pi]),
-grid on, xlim([-pi pi]), ylim([0 10]), xlabel('t'), ylabel('f_s');hold on;
-end
-
 figure('Name',"Fourier series transformation of x")
 for l=1:5
 subplot(3,2,l),
+fplot(t,x,[-pi,pi]), hold on;
 fplot(t,f_s_c(l),[-pi,pi]), title("For n= "+l),
-grid on, xlim([-pi pi]), ylim([0 10]), xlabel('t'), ylabel('f_s');
+grid on, xlim([-pi pi]), ylim([0 10]), xlabel('t'), ylabel('f_s'),legend('x','f_s_c');
 end
 
 end
